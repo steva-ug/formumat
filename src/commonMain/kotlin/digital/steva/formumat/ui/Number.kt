793e9e6bff2eb92dc53838abb0c2479c1feab2b0
Dirk Grappendorf <dg@steva.digital>
2024-07-12 18:39:40 +0200

    feat: in number fields correctly handle the input of "-", ".|," and trailing decimal zeroes (DOT-114)

--- src/commonMain/kotlin/digital/steva/formumat/ui/Number.kt
+++ src/commonMain/kotlin/digital/steva/formumat/ui/Number.kt
@@ -1,5 +1,6 @@
 package digital.steva.formumat.ui
 
+import LocaleSymbols
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -8,12 +9,14 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.unit.dp
-import dev.icerock.moko.resources.compose.stringResource
-import digital.steva.formumat.helpers.Convert
-import digital.steva.formumat.public.MR
 import digital.steva.formumat.redux.ClearValue
 import digital.steva.formumat.redux.Dispatcher
 import digital.steva.formumat.redux.FormumatValues
@@ -111,23 +114,41 @@ fun NumberView(
     enabled: Boolean,
     modifier: Modifier
 ) {
+    fun formatValue(value: Any?, decimalSeparator: Char): String {
+        if (value !is Double) return ""
+        val intValue = value.toInt()
+        if (intValue.toDouble() == value) return intValue.toString()
+        return value.toString().replace('.', decimalSeparator)
+    }
+
+    val decimalSeparator = LocaleSymbols.ofLanguage(Locale.current.language).decimalSeparator
     val property = numberField.property?.eval(values) ?: ""
     val label = numberField.title.eval(values)
     val fieldEnabled = enabled && numberField.enabled.eval(values)
-    var text = Convert.toString(values[property])
+    var onlyMinus by remember { mutableStateOf(false) }
+    var endsWithDot by remember { mutableStateOf(false) }
+    var trailingDezimalZeroes by remember { mutableStateOf(0) }
     val defaultValue = values.getDefault(property)
-    val valueString = values.getWithoutDefault(property)?.toString() ?: ""
+    val valueString = (if (onlyMinus) "-" else formatValue(values.getWithoutDefault(property), decimalSeparator)) +
+            (if (endsWithDot) decimalSeparator else "") +
+            ("0".repeat(trailingDezimalZeroes))
     val defaultValueString = defaultValue?.toString() ?: ""
 
     OutlinedTextField(
-        value = text,
+        value = valueString,
         onValueChange = {
-            text = it
             if (it.isBlank()) {
                 dispatch(ClearValue(property, values.listContext))
+                onlyMinus = false
+                endsWithDot = false
+                trailingDezimalZeroes = 0
             } else {
-                it.toDoubleOrNull()
-                    ?.let { value -> dispatch(SetValue(property, value, values.listContext)) }
+                val text = it.replace(decimalSeparator, '.')
+                onlyMinus = text == "-"
+                endsWithDot = Regex("^[^.]*?\\.+0*$").matches(text)
+                trailingDezimalZeroes = Regex("^.*?\\..*?(0*)$").find(text)?.groupValues?.get(1)?.length ?: 0
+                println(text)
+                text.toDoubleOrNull()?.let { dispatch(SetValue(property, it, values.listContext)) }
             }
         },
         visualTransformation = visualTransformToPlaceholderIfEmpty(valueString, defaultValueString),
